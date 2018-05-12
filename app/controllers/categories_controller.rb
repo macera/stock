98951f5db07deaaac69aded5b622d7c2ac8f84cd
Sera <sera@netlab.jp>
2018-05-12 20:19:16 +0900

    fix categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.order(:group_id)
   end
 
   # GET /categories/1
